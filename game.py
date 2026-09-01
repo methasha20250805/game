a87a3bd1538d6eaeb99a27d25a7a5d023db45f4d
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-09-01 20:13:40 +0530

    Initial commit

--- game.py
+++ game.py
@@ -5,3 +5,10 @@ from datetime import datetime
 USERS_FILE = "users.csv"
 GAMES_FILE = "games.csv"
 REGISTRATIONS_FILE = "registrations.csv"
+
+def ensure_file(filename, headers):
+    """Create the CSV file with headers if it doesn't already exist."""
+    if not os.path.exists(filename):
+        with open(filename, "w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
