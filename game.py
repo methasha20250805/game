6978554ca02e3574046313f71396df70c1249912
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-09-04 22:28:00 +0530

    Adding comments

--- game.py
+++ game.py
@@ -59,6 +59,10 @@ USER_HEADERS = ["user_id", "name", "email"]
 GAME_HEADERS = ["game_id", "name", "genre"]
 REG_HEADERS = ["reg_id", "user_id", "game_id", "timestamp"]
 
+# ---------------------------------------------------------------------------
+# User management
+# ---------------------------------------------------------------------------
+
 def init_files():
     ensure_file(USERS_FILE, USER_HEADERS)
     ensure_file(GAMES_FILE, GAME_HEADERS)
